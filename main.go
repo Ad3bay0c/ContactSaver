4eb3ca30fc6e618b035be5333b4a889b0423a553
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-04 00:33:57 +0100

    Entry Codes Created

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package ContactKeeper
+package main
+
+import (
+	"github.com/Ad3bay0c/ContactSaver/server"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("There was an error loading.env file")
+	}
+
+	newServer := &server.Server{}
+
+	newServer.Start()
+}
