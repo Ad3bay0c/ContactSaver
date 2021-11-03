a79d0aa7154f4d24e9af8a50354c765a57eb5509
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-04 00:34:17 +0100

    Signup handler created

--- server/auth_handlers.go
+++ server/auth_handlers.go
@@ -1,1 +1,12 @@
 package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (s *Server) SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Working Perfectly"})
+	}
+}
