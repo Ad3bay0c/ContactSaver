fdcf7a393166be5bf5a04d57d2cf07e3107a3594
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-04 03:33:54 +0100

    User struct Modified

--- models/user.go
+++ models/user.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type User struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name      string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email     string             `json:"email,omitempty" bson:"email,omitempty" binding:"required"`
+	Password  string             `json:"password,omitempty" bson:"password,omitempty" binding:"required"`
+	CreatedAt time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
+}
