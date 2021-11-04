2bab2d85ef5c1abbdaed9b4fed5b3bd0ae9e4275
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-04 03:38:42 +0100

    Custom responses added for uniformity

--- server/responses/json.go
+++ server/responses/json.go
@@ -6,12 +6,12 @@ import (
 	"time"
 )
 
-func JSON(c *gin.Context,code int, message interface{}, err string, data interface{}) {
+func JSON(c *gin.Context, code int, message string, err interface{}, data interface{}) {
 	response := gin.H{
-		"Error": err,
-		"message": message,
-		"data": data,
-		"status": http.StatusText(code),
+		"Error":      err,
+		"message":    message,
+		"data":       data,
+		"status":     http.StatusText(code),
 		"time_stamp": time.Now().Format("2006-01-02 15:04:05"),
 	}
 	c.JSON(code, response)
