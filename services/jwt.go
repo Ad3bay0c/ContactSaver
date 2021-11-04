8a4dc970c72cf95b42593b5602c2e31b5af05cfa
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-04 03:37:01 +0100

    Fucntions to create token ad parse token added

--- services/jwt.go
+++ services/jwt.go
@@ -1,1 +1,24 @@
 package services
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"os"
+	"time"
+)
+
+func GenerateToken(userID interface{}) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"Authorized": true,
+		"expiresAt": time.Now().Add(time.Hour * 1).Unix(),
+		"issuedAt": time.Now().Unix(),
+		"userId": userID,
+	})
+
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func VerifyToken(token string) (*jwt.Token, error){
+	return jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return os.Getenv(os.Getenv("JWT_SECRET")), nil
+	})
+}
