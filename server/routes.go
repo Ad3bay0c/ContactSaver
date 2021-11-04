745b7617159f2a45d3fc8ad8e1dd2d3261739f13
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-04 03:34:42 +0100

    Login and signup routes added

--- server/routes.go
+++ server/routes.go
@@ -1,7 +1,29 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func (s *Server) Routes(router *gin.Engine) {
-	router.GET("/", s.SignUp())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"POST", "GET", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		// AllowOriginFunc: func(origin string) bool {
+		// 	return origin == "https://github.com"
+		// },
+		MaxAge: 12 * time.Hour,
+	}))
+
+	AuthRouter := router.Group("/api/auth")
+	UserRouter := router.Group("/api/user")
+	//ContactRouter := UserRouter.Group("/contact")
+	AuthRouter.POST("/", s.SignUp())
+	AuthRouter.POST("/login", s.Login())
+
+	UserRouter.GET("/", s.GetUser())
 }
