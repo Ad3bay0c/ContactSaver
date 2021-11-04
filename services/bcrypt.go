5ab1a596e402f788982f767179024b4f296d01b8
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-04 03:35:59 +0100

    Bcrypt methods for hashing and verifying passwords Added

--- services/bcrypt.go
+++ services/bcrypt.go
@@ -1,1 +1,11 @@
 package services
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func ComparePassword(password, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
