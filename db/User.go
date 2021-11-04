24fa72ca6f22a97f19655b3da1b8a9328f0b8410
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-04 03:33:25 +0100

    Three nethods added to User DB Acces

--- db/User.go
+++ db/User.go
@@ -1,1 +1,23 @@
 package db
+
+import (
+	"context"
+	"github.com/Ad3bay0c/ContactSaver/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"time"
+)
+
+func (mongodb *MongoDB) CreateUser(user *models.User) (interface{}, error) {
+	collection := mongodb.InitializeCollection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+	result, err := collection.InsertOne(ctx, user)
+	return result.InsertedID, err
+}
+
+func (mongodb *MongoDB) GetUserByEmail(email string) (*models.User, bool) {
+	user := &models.User{}
+	collection := mongodb.InitializeCollection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+	err := collection.FindOne(ctx, bson.M{"email": email}).Decode(user)
+	return user, err == nil
+}
