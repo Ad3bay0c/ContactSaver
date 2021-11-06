c548fd5afa47aafacad4261e867cbeceaf2b6c86
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-06 16:08:50 +0100

    Contact Model Added'

--- models/contact.go
+++ models/contact.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Contact struct {
+	ID		primitive.ObjectID	`json:"id,omitempty" bson:"_id,omitempty"`
+	UserID	string	`json:"user_id,omitempty" bson:"user_id"`
+	Name	string	`json:"name" bson:"name" binding:"required"`
+	Email	string	`json:"email,omitempty" bson:"email,omitempty" binding:"required"`
+	Phone	string	`json:"phone,omitempty" bson:"phone,omitempty" binding:"required"`
+	Type	string	`json:"type,omitempty" bson:"type,omitempty"`
+	Date	time.Time `json:"date,omitempty" bson:"date,omitempty"`
+}
