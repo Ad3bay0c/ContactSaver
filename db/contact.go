7c2e69b963931f8c937a0f184b15508fd44382cb
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-06 16:03:01 +0100

    Get all contacts for a particular user db access method added

--- db/contact.go
+++ db/contact.go
@@ -3,6 +3,9 @@ package db
 import (
 	"context"
 	"github.com/Ad3bay0c/ContactSaver/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"time"
 )
 
@@ -13,3 +16,17 @@ func (mongodb *MongoDB) CreateContact(contact *models.Contact) error {
 	_, err := collection.InsertOne(ctx, contact)
 	return err
 }
+func (mongodb *MongoDB) GetAllContacts(userID string) ([]models.Contact, error) {
+	var contacts []models.Contact
+	collection := mongodb.InitializeCollection("contactss")
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer cancelFunc()
+	Opt := options.Find().SetSort(bson.D{{"date", -1}})
+	res, err := collection.Find(ctx, bson.M{"user_id": userID}, Opt)
+	if err != nil {
+		return nil, err
+	}
+	err = res.All(ctx, &contacts)
+	return contacts, err
+}
+
