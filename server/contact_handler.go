11b388caa03a0f591e6526f51044043f25f7378d
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-06 16:10:52 +0100

    Create Contact Handler Created

--- server/contact_handler.go
+++ server/contact_handler.go
@@ -1,9 +1,36 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Ad3bay0c/ContactSaver/models"
+	"github.com/Ad3bay0c/ContactSaver/server/responses"
+	"github.com/Ad3bay0c/ContactSaver/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
 
 func (s *Server) CreateContact() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		userID, ok := c.Get("userID")
+		if !ok {
+			responses.JSON(c, http.StatusInternalServerError, "", "Internal Server Error", nil)
+			return
+		}
+		ID := userID.(string)
+		contact := &models.Contact{}
+		errs:= services.Decode(c, contact)
+		if errs != nil {
+			responses.JSON(c, http.StatusBadRequest, "", errs, nil)
+			return
+		}
+		contact.UserID = ID
+		contact.Date = time.Now()
+		err := s.DB.CreateContact(contact)
+		if err != nil {
+			responses.JSON(c, http.StatusBadRequest, "", "Error Creating Contact", nil)
+			return
+		}
+		responses.JSON(c, http.StatusCreated, "Contact Created Successfully", "", nil)
+		return
 	}
 }
