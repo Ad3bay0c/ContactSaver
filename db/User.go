d2b1ac7c95cd02f07d65551bc385959977c3f380
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-06 16:01:11 +0100

    Get Authorized user's Information DB Access Method Created

--- db/User.go
+++ db/User.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/Ad3bay0c/ContactSaver/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 )
 
@@ -20,4 +21,14 @@ func (mongodb *MongoDB) GetUserByEmail(email string) (*models.User, bool) {
 	ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
 	err := collection.FindOne(ctx, bson.M{"email": email}).Decode(user)
 	return user, err == nil
+}
+
+func (mongodb *MongoDB) GetAuthUser(id string) (*models.User, error){
+	user := &models.User{}
+	collection := mongodb.InitializeCollection("user")
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 30 * time.Second)
+	newId, _ := primitive.ObjectIDFromHex(id) //create a new ObjectID from the string
+	err := collection.FindOne(ctx, bson.M{"_id": newId}).Decode(user)
+	cancelFunc()
+	return user, err
 }
