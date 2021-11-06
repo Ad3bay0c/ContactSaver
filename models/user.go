16959f08746948aa72af7a359d1829d36007d7f8
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-06 16:09:25 +0100

    User Struct Modified

--- models/user.go
+++ models/user.go
@@ -9,6 +9,7 @@ type User struct {
 	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Name      string             `json:"name,omitempty" bson:"name,omitempty"`
 	Email     string             `json:"email,omitempty" bson:"email,omitempty" binding:"required"`
-	Password  string             `json:"password,omitempty" bson:"password,omitempty" binding:"required"`
+	Password  string             `json:"password,omitempty" bson:"-" binding:"required"`
+	PasswordHash string			`json:"-" bson:"password"`
 	CreatedAt time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
