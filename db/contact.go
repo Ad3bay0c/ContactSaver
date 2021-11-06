1e14b2653ee0f8887c795ccc98cb512743eb0115
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-06 16:02:24 +0100

    Create a contact db access method added

--- db/contact.go
+++ db/contact.go
@@ -1,1 +1,15 @@
 package db
+
+import (
+	"context"
+	"github.com/Ad3bay0c/ContactSaver/models"
+	"time"
+)
+
+func (mongodb *MongoDB) CreateContact(contact *models.Contact) error {
+	collection := mongodb.InitializeCollection("contactss")
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer cancelFunc()
+	_, err := collection.InsertOne(ctx, contact)
+	return err
+}
